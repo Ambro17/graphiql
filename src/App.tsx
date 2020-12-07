2bd469da2c06493eb407efa5a7c94500650be9b8
Ambro17 <ambro17.1@gmail.com>
2020-12-07 20:10:00 -0300

    🔧 Forgive me father for i have sinned. 🚧 First working delegation to graphiql component. This for sure has room for improvement

--- src/App.tsx
+++ src/App.tsx
@@ -3,43 +3,49 @@ import GraphiQL from 'graphiql';
 import 'graphiql/graphiql.min.css';
 
 class App extends React.Component {
+    _graphiql: any;
+
     render() {
-      return (
-        <GraphiQL
-        fetcher={async graphQLParams => {
-        const data = await fetch(
-            'https://swapi-graphql.netlify.app/.netlify/functions/index',
-            {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(graphQLParams),
-            credentials: 'same-origin',
-            },
-        );
-        return data.json().catch(() => data.text());
-        }}
-    >
-        <GraphiQL.Logo>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-            <div>
-                <img
-                alt="PostGraphile logo"
-                src="https://www.graphile.org/images/postgraphile-tiny.optimized.svg"
-                width="32"
-                height="32"
-                style={{ marginTop: '4px', marginRight: '0.5rem' }}
-                />
-            </div>
-            <div>
-                Graph
-                <em>i</em>
-                QL
-            </div>
-            </div>
-        </GraphiQL.Logo>
+        return (
+            <GraphiQL
+                ref={ref => (this._graphiql = ref)}
+                fetcher={async graphQLParams => {
+                const data = await fetch(
+                    'https://swapi-graphql.netlify.app/.netlify/functions/index',
+                    {
+                    method: 'POST',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(graphQLParams),
+                    credentials: 'same-origin',
+                    },
+                );
+                return data.json().catch(() => data.text());
+            }}
+            >
+
+            {/* ------- Logo ---------- */}
+            <GraphiQL.Logo>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                <div>
+                    <img
+                    alt="PostGraphile logo"
+                    src="https://www.graphile.org/images/postgraphile-tiny.optimized.svg"
+                    width="32"
+                    height="32"
+                    style={{ marginTop: '4px', marginRight: '0.5rem' }}
+                    />
+                </div>
+                <div>
+                    Graph
+                    <em>i</em>
+                    QL
+                </div>
+                </div>
+            </GraphiQL.Logo>
+        {/* ------- Buttons ---------- */}
         {
             /*
             Prettify
@@ -50,12 +56,14 @@ class App extends React.Component {
             Voyager
             */
         }
-        {/* <GraphiQL.Toolbar>
+        <GraphiQL.Toolbar>
             <GraphiQL.Button
                 onClick={() => this._graphiql.handlePrettifyQuery()}
-                label="Prettify"
                 title="Prettify Query (Shift-Ctrl-P)"
+                label="Prettify"
             />
+        </GraphiQL.Toolbar>
+        {/*
             <GraphiQL.Button
                 onClick={() => this._graphiql.handleToggleHistory()}
                 label="History"
